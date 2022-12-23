b3f676e983d0b54bf6b76bd36322a3f2b44263f6
Andrea Lobo <andrea.lobo@pix4d.com>
2022-12-23 11:58:55 +0100

    Receive multiple PINGS

--- app/main.py
+++ app/main.py
@@ -6,8 +6,12 @@ def main():
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     client_connection, _ = server_socket.accept()
-    client_connection.recv(1024)
-    client_connection.send(b"+PONG\r\n")
+
+    while True:
+        client_connection.recv(1024)
+        client_connection.send(b"+PONG\r\n")
+
+
 
 
 
